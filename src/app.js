e7b634eaac4216077a01d2b17c3c9c6841e9b48e
pawansankhle <>
2019-07-24 18:11:34 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+
+import { sumFun } from './js';
+
+
+console.log("Result: " + sumFun(2,3));
+console.log("Result: " + sumFun(2,5));
---
+++ src/js/lib.js
@@ -0,0 +1,5 @@
+
+const sumFun = (val1, val2) => val1 + val2;
+
+
+export { sumFun }
