21fdf7e6ec02615b2e9c8eb601838ec0df45b6c2
pawansankhle <>
2019-07-26 18:04:05 +0530

    changes

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: {
@@ -21,16 +22,48 @@ module.exports = {
                         presets: ["babel-preset-env"]
                     }
                 }
-            }
+            },
+            {
+                // Apply rule for .sass, .scss or .css files
+                test: "/\.(sa|sc|c)ss$/",
+          
+                // Set loaders to transform files.
+                // Loaders are applying from right to left(!)
+                // The first loader will be applied after others
+                use: [
+                     {
+                        // After all CSS loaders we use plugin to do his work.
+                        // It gets all transformed CSS and extracts it into separate
+                        // single bundled file
+                        loader: MiniCssExtractPlugin.loader
+                      }, 
+                       {
+                         // This loader resolves url() and @imports inside CSS
+                         loader: "css-loader",
+                       },
+                       {
+                         // Then we apply postCSS fixes like autoprefixer and minifying
+                         loader: "postcss-loader"
+                       },
+                       {
+                         // First we transform SASS to standard CSS
+                         loader: "sass-loader",
+                         options: {implementation: require("sass")}
+                       }
+                    ]
+            }]
+        },
+        plugins: [
+            new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ['dist']
+            }),
+            new HtmlWebpackPlugin({
+                title: 'demo webpack App',
+            }),
+            new MiniCssExtractPlugin({
+                filename: "bundle.css"
+              })
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin({
-        cleanAfterEveryBuildPatterns: ['dist']
-        }),
-        new HtmlWebpackPlugin({
-            title: 'demo webpack App',
-        })
-            ]
+        
     
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,25 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  module : {
+    rule: [
+      {
+        // Now we apply rule for images
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+               {
+                 // Using file-loader for these files
+                 loader: "file-loader",
+  
+                 // In options we can set different things like format
+                 // and directory to save
+                 options: {
+                   outputPath: 'fonts'
+                 }
+               }
+             ]
+      }
+    ]
+  }
 })
